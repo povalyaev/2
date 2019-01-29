97811298c4e190443890ed9ab473b67c4059e28d
Yevhen Povaliaiev <eugene.povalyaev@gmail.com>
2019-01-29 21:23:58 +0200

    output all variables

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php 
-echo "Server name - ".$_SERVER['SERVER_NAME']."<br />"; 
-echo "Path - ".$_SERVER['DOCUMENT_ROOT']."<br />"; 
-echo "IP - ".$_SERVER['SERVER_ADDR']."<br />"; 
-echo "Port number - ".$_SERVER['SERVER_PORT']."<br />"; 
-echo "Software server - ".$_SERVER['SERVER_SOFTWARE']."<br />"; 
-
+echo '<pre>';
+print_r( $_SERVER );
+echo '</pre>';
 
 
