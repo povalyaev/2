8700a09b47552e29658baea8ca2a70fca50ffb82
Yevhen Povaliaiev <eugene.povalyaev@gmail.com>
2019-01-29 18:29:18 +0200

    add 2.1-2.3 php file

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php
-echo $_SERVER;
+<?php 
+  echo "Server name - ".$_SERVER['SERVER_NAME']."<br />"; 
+  echo "Path - ".$_SERVER['DOCUMENT_ROOT']."<br />"; 
+  echo "IP - ".$_SERVER['SERVER_ADDR']."<br />"; 
+  echo "Port number - ".$_SERVER['SERVER_PORT']."<br />"; 
+  echo "Software server - ".$_SERVER['SERVER_SOFTWARE']."<br />"; 
+
 
 
