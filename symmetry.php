5f91f81711492f3f39f4ce20c861b6b74051a4da
Yevhen Povaliaiev <eugene.povalyaev@gmail.com>
2019-02-18 13:55:16 +0200

    Create symmetry.php

---
+++ symmetry.php
@@ -0,0 +1,57 @@
+<?php
+
+class stack
+{
+    private $top = 0;
+    private $hranilishche = [];
+
+    public function in($value)
+    {
+        $this->hranilishche[$this->top++] = $value;
+    }
+
+    public function out()
+    {
+        if($this->isEmpty()) {
+            throw new RuntimeException("Stack is empty!");
+        }
+        return $this->hranilishche[--$this->top];
+    }
+
+    public function isEmpty()
+    {
+        return $this->top === 0;
+    }
+}
+
+function testing($symmetry)
+{
+    $bracket = ['[' => ']', '<' => '>', '{' => '}', '(' => ')'];
+    $symmetryok = new stack();
+    $symmetryoff = false;
+    $length = strlen($symmetry);
+
+    for ($i = 0; $i < $length; $i++) {
+        if (array_key_exists($symmetry[$i], $bracket)) {
+            $symmetryok->in($bracket[$symmetry[$i]]);
+        } else{
+            $symmetryoff .= $symmetryok->out();
+        }
+    }
+
+    if ($symmetryoff === substr($symmetry, $length/2)) {
+        echo substr($symmetry, $length/2) . ' === ';
+        echo $symmetryoff . PHP_EOL;
+        return true;
+    } else {
+        echo substr($symmetry, $length/2) . ' === ';
+        echo $symmetryoff . PHP_EOL;
+        return false;
+    }
+}
+
+var_dump(testing("(<{}>)"));
+var_dump(testing("[(<{}>)]"));
+var_dump(testing("(<{)"));
+var_dump(testing("[(<{}>])"));
+
